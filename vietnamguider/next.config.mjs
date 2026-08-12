fdeec59533ee9cdc6b3bd57f450fb9a6bf0cf842
viecnha247vn <viecnha247.vn@gmail.com>
2026-08-12 09:03:43 +0200

    Delete vietnamguider directory

--- vietnamguider/next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    // Add remote hosts here when you start embedding hotel/partner images.
-    remotePatterns: [],
-  },
-};
-
-export default nextConfig;
